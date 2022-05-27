b160b520688be9f70a7f5d3cf717f45d38ca0f93
yufus7 <79915956+yufus7@users.noreply.github.com>
2022-05-27 16:02:17 +0300

    Create odev4.sql

---
+++ odev4.sql
@@ -0,0 +1,30 @@
+--1
+SELECT DISTINCT(replacement_cost) 
+FROM film;
+
+
+--2
+SELECT COUNT(DISTINCT(replacement_cost) )
+FROM film;
+
+
+--3
+SELECT COUNT(DISTINCT(title) )
+FROM film
+WHERE title
+LIKE 'T%'
+AND rating = 'G';
+
+
+--4
+SELECT COUNT( country )
+FROM country
+WHERE country
+LIKE '_____';
+
+
+--5
+SELECT COUNT(city )
+FROM city
+WHERE city
+ILIKE '%r';
